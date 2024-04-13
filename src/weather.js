876e93bc5451623d5f6a1c5e56ac62b8cf823352
krystleL21 <54385637+krystleL21@users.noreply.github.com>
2024-04-13 01:12:47 -0400

    updated CSS and javascript weather api

--- src/weather.js
+++ src/weather.js
@@ -10,13 +10,17 @@ function refreshWeather(response) {
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url} "class="weather-app-icon"/>`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url} "class="weather-app-icon" />`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${humidityT}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+
+  getForecast(response.data.city);
+
+
 }
 
 function formatDate(date) {
@@ -32,13 +36,13 @@ function formatDate(date) {
     "Saturday",
   ];
 
-  let day = days[date.getDay()];
+  let day1 = days[date.getDay()];
 
   if (minutes < 10) {
     miinutes = `0${minutes}`;
   }
 
-  return `${day} ${hours}:${minutes}`;
+  return `${day1} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -54,9 +58,50 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+return days[date.getDay()];
+}
+
+function getForecast(city) {
+ let apiKey = "b68t905a51397a01bf1fcdf4ao4dde24";
+ let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+ axios(apiUrl).then(displayForecast);
+}
+
+
+function displayForecast(response) {
+console.log(response.data);
+
+let forecastElement = document.querySelector("#forecast");
+
+let forecastHtml ="";
+
+response.data.daily.forEach(function (day, index) {
+if (index < 5) {
+forecastHtml =
+  forecastHtml +
+  `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>${Math.round(day.temperature.maximum)}°</strong>
+      </div>
+      <div class="weather-forecast-temperature">${Math.round(
+        day.temperature.minimum
+      )}°</div>
+    </div>
+  </div>
+
+`;
+}});
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Kingston");
-
-
